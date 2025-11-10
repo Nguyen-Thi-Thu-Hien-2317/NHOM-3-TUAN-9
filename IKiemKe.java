3f98f9b0fc677c03ffe3f0c6a7b8a1cf4ec1ace6
Nguyen-Thi-Thu-Hien-2317 <thuhien3176@gmail.com>
2025-11-11 00:37:54 +0700

    Update IKiemKe.java
    Ghi chú thêm

--- IKiemKe.java
+++ IKiemKe.java
@@ -1,3 +1,5 @@
+// Interface thể hiện tính ""trừu tượng"
+// Định nghĩa hành vi kiểm kê và cập nhật vị trí cho mọi loại sách
 public interface IKiemKe {
     boolean kiemTraTonKho(int soLuongToiThieu);
 
