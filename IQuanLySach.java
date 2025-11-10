ce8f85cf0ca3faab1e5bedb0e56993e19adf819d
Nguyen-Thi-Thu-Hien-2317 <thuhien3176@gmail.com>
2025-11-11 00:38:28 +0700

    Update IQuanLySach.java
    Ghi chú và hoàn thiện code

--- IQuanLySach.java
+++ IQuanLySach.java
@@ -1,19 +1,29 @@
+
+// Interface thể hiện tính "trừu tượng""
+// Xác định các hành vi cơ bản của một hệ thống quản lý sách
 import java.util.List;
 
 public interface IQuanLySach {
-    void themSach(Sach s);
+    void themSach(SachTuan9 s);
 
-    Sach timKiemTheoMa(String maSach);
+    SachTuan9 timKiemTheoMa(String maSach);
 
-    List<Sach> timKiemTheoTieuDe(String tuKhoa);
+    List<SachTuan9> timKiemTheoTieuDe(String tuKhoa);
 
-    List<Sach> timKiemTheoTacGia(String tenTacGia);
+    List<SachTuan9> timKiemTheoTacGia(String tenTacGia);
 
     boolean xoaSach(String maSach);
 
+    boolean capNhatSach(String maSach, SachTuan9 thongTinMoi);
+
     void hienThiTatCa();
 
-    List<Sach> getDanhSach();
+    // Thêm mới các chức năng được yêu cầu trong nhận xét:
+    void sapXepTheoGia(boolean tangDan);
 
-    boolean capNhatSach(String maSach, Sach thongTinMoi);
+    double tinhTongGiaTriKho();
+
+    void luuCSV(String filePath);
+
+    void napTuCSV(String filePath);
 }
